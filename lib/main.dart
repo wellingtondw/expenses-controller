d9db15547ee827c763e55459a14652cf91930124
Wellington <wellingtonslpbi@hotmail.com>
2020-03-10 09:09:54 -0300

    string interpolate and fixed to string

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class MyHomePage extends StatelessWidget {
                       decoration: BoxDecoration(
                           border: Border.all(color: Colors.purple, width: 2)),
                       padding: EdgeInsets.all(10),
-                      child: Text(tr.value.toString(),
+                      child: Text('R\$ ${tr.value.toStringAsFixed(2)}',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
